225e1de38adb724b62419fac447ff762c1cf62fb
wonjun0120 <wonjundero@naver.com>
2020-04-09 11:50:33 +0900

    유효성 검사 실패시 실패 문구 출력

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 let userData = document.userData
-
+//firstName 유효성 검사
 function formValidFirstName(){
     let firstName = userData.firstName;
     let getFirstName = document.querySelector('#getFirstName');
@@ -9,9 +9,15 @@ function formValidFirstName(){
     newSpan.innerHTML = '&#x2717;'; //X
     if(firstName.value.length !== 0){
         newSpan.innerHTML = '&#x2713;'  //V
+        getFirstName.appendChild(newSpan);
+    }else{
+        let newErrorSpan = document.createElement('span');
+        newErrorSpan.innerHTML = 'please give us a vaild name';
+        getFirstName.appendChild(newSpan);
+        getFirstName.appendChild(newErrorSpan);
     }
-    getFirstName.appendChild(newSpan);
 }
+//lastName유효성 검사
 function formValidLastName(){
     let lastName = userData.lastName;
     let getLastName = document.querySelector('#getLastName');
@@ -20,23 +26,45 @@ function formValidLastName(){
     newSpan.innerHTML = '&#x2717;'; //X
     if(lastName.value.length !== 0){
         newSpan.innerHTML = '&#x2713;'  //V
+        getLastName.appendChild(newSpan);
+    }else{
+        let newErrorSpan = document.createElement('span');
+        newErrorSpan.innerHTML = 'please give us a vaild name';
+        getLastName.appendChild(newSpan);
+        getLastName.appendChild(newErrorSpan);
     }
-    getLastName.appendChild(newSpan);
 }
+//전번 유효성 검사
 function formValidPhoneNum(){
     let phoneNum = userData.phoneNum;
     let altPhoneNum = userData.altrPhoneNum;
     let getPhoneNum = document.querySelector('#getPhoneNum');
     let getAltPhoneNum = document.querySelector('#getAltPhoneNum');
     let newSpan = document.createElement('span');
+    //console.log(typeof(getPhoneNum.value));
     newSpan.innerHTML = '&#x2717;'; //X
-
-    if(phoneNum.value === altPhoneNum.value){
-        newSpan.innerHTML = '&#x2713;'  //V
+    if(phoneNum.value.length !== 0){
+        if(phoneNum.value === altPhoneNum.value){
+            newSpan.innerHTML = '&#x2713;'  //V
+            getAltPhoneNum.appendChild(newSpan);
+        }else{
+            let newErrorSpan = document.createElement('span');
+            newErrorSpan.innerHTML = 'Those phone numbers don\'t match. Please check and try again';
+            getAltPhoneNum.appendChild(newSpan);
+            getAltPhoneNum.appendChild(newErrorSpan);
+        }
+    }else{
+        let newErrorSpan = document.createElement('span');
+        let newSpan2 = document.createElement('span');
+        newSpan2.innerHTML = '&#x2717;'; //X
+        newErrorSpan.innerHTML = 'please give us a vaild phone number';
+        getPhoneNum.appendChild(newSpan);
+        getPhoneNum.appendChild(newErrorSpan);
+        getAltPhoneNum.appendChild(newSpan2);
     }
-    getPhoneNum.appendChild(newSpan);
-    getAltPhoneNum.appendChild(newSpan);
+
 }
+//이메일 유효성 검사
 function formValidEmail(){
     let emailAdd = userData.emailAdd;
     let getEmailAdd = document.querySelector('#getEmailAdd');
@@ -44,8 +72,14 @@ function formValidEmail(){
     newSpan.innerHTML = '&#x2717;'; //X
     if(emailAdd.value.length !== 0){
         newSpan.innerHTML = '&#x2713;'  //V
+        getEmailAdd.appendChild(newSpan);
+    }else{
+        let newErrorSpan = document.createElement('span');
+        newErrorSpan.innerHTML = 'please give us a vaild email address';
+        getEmailAdd.appendChild(newSpan);
+        getEmailAdd.appendChild(newErrorSpan);
     }
-    getEmailAdd.appendChild(newSpan);
+   
 }
 
 //유효성 검사
